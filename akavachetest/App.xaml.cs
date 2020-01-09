80bee5feacc5edbfd471ef160302a7d9113dfc83
Albilaga <albilaga.lp@gmail.com>
2020-01-09 09:34:42 +0700

    Add Akavache.

--- akavachetest/App.xaml.cs
+++ akavachetest/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Akavache;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace akavachetest
 {
@@ -9,13 +12,37 @@ namespace akavachetest
         public App()
         {
             InitializeComponent();
+            Registrations.Start("TEST");
+            //var token = GetToken().Result;
+            //MainPage = new MainPage();
+            SetMainPage();
+        }
 
+        protected override async void OnStart()
+        {
+            // Handle when your app starts
+          
+        }
+
+        private async void SetMainPage()
+        {
+            var token = await GetToken();
             MainPage = new MainPage();
         }
 
-        protected override void OnStart()
+        private async Task<string> GetToken()
         {
-            // Handle when your app starts
+            try
+            {
+                var token = await BlobCache.Secure.GetObject<string>("access_token");
+                Debug.WriteLine($"Token: {token}");
+                return token;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Error: {e.Message}");
+                return null;
+            }
         }
 
         protected override void OnSleep()
